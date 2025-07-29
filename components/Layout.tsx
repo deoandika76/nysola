f76db57214e59482c4d39a5be0b0f4046315a05d
deoandika76 <66901499+deoandika76@users.noreply.github.com>
2025-07-29 10:35:34 +0700

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,9 +2,11 @@ import Sidebar from './Sidebar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
       <Sidebar />
-      <main className="ml-64 flex-1 p-8 bg-carbon text-white min-h-screen">{children}</main>
+      <main className="md:ml-64 w-full p-4 sm:p-6 md:p-8 bg-carbon text-white min-h-screen">
+        {children}
+      </main>
     </div>
   );
 }
