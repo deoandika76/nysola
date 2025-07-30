9a897af779d75fc1e0fe8715910e7c7285ebcb1b
deoandika76 <66901499+deoandika76@users.noreply.github.com>
2025-07-30 13:04:36 +0700

    Update logNotification.ts

--- pages/api/logNotification.ts
+++ pages/api/logNotification.ts
@@ -1,11 +1,10 @@
-// pages/api/logNotification.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { db } from '../../firebase'; // ← pastikan path sesuai
+import { db } from '../../firebase';
 import { collection, addDoc, Timestamp } from 'firebase/firestore';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Only POST requests allowed' });
+    return res.status(405).json({ message: 'Only POST allowed' });
   }
 
   try {
