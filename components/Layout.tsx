4a4b26b2bb0ce56515388088f722f1ec4279ef77
deoandika76 <66901499+deoandika76@users.noreply.github.com>
2025-08-03 05:00:33 +0700

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col md:flex-row">
       <Sidebar isOpen={true} />
-      <Navbar isOpen={true} />
+      <Navbar isOpen={navOpen} onClose={() => setNavOpen(false)} />
       <main className="md:ml-64 w-full p-4 sm:p-6 md:p-8 bg-carbon text-white min-h-screen">
         {children}
       </main>
