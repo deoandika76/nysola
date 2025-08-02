0dfe9b01aa3e97800b4ec93d32dc00c894192827
deoandika76 <66901499+deoandika76@users.noreply.github.com>
2025-08-03 04:49:12 +0700

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,12 @@
+// components/Layout.tsx
 import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col md:flex-row">
       <Sidebar />
+      <Navbar isOpen={true} /> {/* ✅ fix properti yang error */}
       <main className="md:ml-64 w-full p-4 sm:p-6 md:p-8 bg-carbon text-white min-h-screen">
         {children}
       </main>
