ee5f1a3b5f4a5aa49ac3992930edc9cd4262098a
deoandika76 <66901499+deoandika76@users.noreply.github.com>
2025-08-05 04:26:17 +0700

    Update cron-execute.ts

--- pages/api/cron-execute.ts
+++ pages/api/cron-execute.ts
@@ -1,17 +1,5 @@
-// pages/api/cron-execute.ts/
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { executeAutoTask } from '../../utils/executor';
+{
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ status: 'error', message: 'Only POST allowed for cron' });
-  }
+// komentar
 
-  try {
-    const result = await executeAutoTask();
-    return res.status(200).json({ status: 'success', message: result });
-  } catch (err: any) {
-    console.error('Cron Executor Error:', err);
-    return res.status(500).json({ status: 'error', message: err.message || 'Unexpected error' });
-  }
 }
