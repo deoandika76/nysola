af212946e12fd906c193e53b6b80f3771b3c809a
deoandika76 <66901499+deoandika76@users.noreply.github.com>
2025-08-07 05:15:53 +0700

    Update DashboardCard.tsx

--- components/DashboardCard.tsx
+++ components/DashboardCard.tsx
@@ -1,5 +1,4 @@
 // components/DashboardCard.tsx
-// cuma iseng wkwk
 type Props = {
   title: string;
   value: string;
@@ -9,7 +8,7 @@ type Props = {
 
 export default function DashboardCard({ title, value, icon, color }: Props) {
   return (
-    <div className="bg-[#1a1a1a] border border-gray-700 p-6 rounded-xl text-center shadow-md w-60">
+    <div className="bg-black/50 backdrop-blur-sm border border-purple-600 p-6 rounded-xl text-center shadow-md w-60">
       <div className="text-3xl mb-2">{icon}</div>
       <h3 className={`text-xl font-bold mb-1 ${color || 'text-cyan'}`}>{value}</h3>
       <p className="text-gray-400 text-sm">{title}</p>
